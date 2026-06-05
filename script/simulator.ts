0e45f2e18f51d6b70106db6de7ca3fdd37d4a058
massylamarni <massyl.amarni@outlook.fr>
2026-06-05 15:43:26 +0200

    updated base_url & title

--- script/simulator.ts
+++ script/simulator.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 const randomInt = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
